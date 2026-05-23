1d54739fa3e3f012f834253c2d27542ecbebc5ee
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-05-23 14:56:00 +0500

    feat: implement folder management with CRUD operations and move functionality

--- src/modules/folders/folders.controller.ts
+++ src/modules/folders/folders.controller.ts
@@ -1,67 +1,121 @@
 import asyncHandler from "../../utils/asyncHandler";
 import { Request, Response } from "express";
 import { ApiResponse } from "../../utils/ApiResponse";
-import { moveFolderService } from "./folders.service";
+import { createFolderService, deleteFolderService, getFolderByIdService, getFoldersService, moveFolderService, updateFolderService } from "./folders.service";
 
+/**
+ * @description Retrieves all folders available in the system.
+ * @route GET /api/tenant/folders
+ * @access Private
+ */
 export const getFolders = asyncHandler(async (req: Request, res: Response) => {
+
+    const folders = await getFoldersService();
+    res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                folders,
+                "Folders retrieved successfully"
+            )
+        );
+});
+
+/**
+ * @description Retrieves a single folder by its unique ID.
+ * @route GET /api/tenant/folders/:id
+ * @access Private
+ */
+export const getFolderById = asyncHandler(async (req: Request, res: Response) => {
+
+    const folderId = Number(req.params.id);
+    const folder = await getFolderByIdService(folderId);
 
     res.status(200)
         .json(
             new ApiResponse(
                 200,
-                "Folders retrieved successfully"
-            ));
-});
-
-export const getFolderById = asyncHandler(async (req: Request, res: Response) => {
-
-    res.status(200)
-        .json(
-            new ApiResponse(
-                200, "Folder retrieved successfully"
+                folder,
+                "Folder retrieved successfully"
             )
         );
 });
 
+/**
+ * @description Creates a new folder in the system.
+ * @route POST /api/tenant/folders
+ * @access Private
+ */
 export const createFolder = asyncHandler(async (req: Request, res: Response) => {
+
+    const folder = req.body;
+    const createdFolder = await createFolderService(folder);
 
     res.status(201)
         .json(
             new ApiResponse(
-                201, "Folder created successfully"
+                201,
+                createdFolder,
+                "Folder created successfully"
             )
         );
 });
 
+/**
+ * @description Updates an existing folder by its unique ID.
+ * @route PATCH /api/tenant/folders/:id
+ * @access Private
+ */
 export const updateFolder = asyncHandler(async (req: Request, res: Response) => {
 
+    const folderId = Number(req.params.id);
+    const folder = req.body;
+    const updatedFolder = await updateFolderService(folderId, folder);
     res.status(200)
         .json(
             new ApiResponse(
-                200, "Folder updated successfully"
+                200,
+                updatedFolder,
+                "Folder updated successfully"
             )
         );
 });
 
+/**
+ * @description Deletes an existing folder by its unique ID.
+ * @route DELETE /api/tenant/folders/:id
+ * @access Private
+ */
 export const deleteFolder = asyncHandler(async (req: Request, res: Response) => {
-
+    const folderId = Number(req.params.id)
+    const deletedFolder = await deleteFolderService(folderId);
     res.status(200)
         .json(
             new ApiResponse(
-                200, "Folder deleted successfully"
+                200,
+                deletedFolder,
+                "Folder deleted successfully"
             )
         );
 });
 
+/**
+ * @description Moves a folder to a new parent folder.
+ * @route POST /api/tenant/folders/:id/move
+ * @access Private
+ */
 export const moveFolder = asyncHandler(async (req: Request, res: Response) => {
 
-    const { id } = req.params;
-    const movedFolder = await moveFolderService(id, req.body);
+    const folderId = Number(req.params.id);
+    const newParentId = req.body.parent_id;
+    const movedFolder = await moveFolderService(folderId, newParentId);
 
     res.status(200)
         .json(
             new ApiResponse(
-                200, movedFolder, "Folder moved successfully"
-            )   
+                200,
+                movedFolder,
+                "Folder moved successfully"
+            )
         );
 });
