c62de6adfc89e0b1a6b4455ef9c41c71a76f8c62
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-06-23 16:36:02 +0500

    fix: update database constraints and enhance user role retrieval with cascade delete

---
+++ prisma/migrations/20260616093147_add_cascade_delete/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "RolePermission" DROP CONSTRAINT "RolePermission_permission_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "RolePermission" DROP CONSTRAINT "RolePermission_role_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserRole" DROP CONSTRAINT "UserRole_role_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserRole" DROP CONSTRAINT "UserRole_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermission" ADD CONSTRAINT "RolePermission_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermission" ADD CONSTRAINT "RolePermission_permission_id_fkey" FOREIGN KEY ("permission_id") REFERENCES "Permission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
