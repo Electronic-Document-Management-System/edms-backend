f89557088d0b9eef95fff7672c5213adb1fefa5c
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-06-25 00:41:10 +0500

    refactor: routes location change

--- src/modules/sharing/sharing.route.ts
+++ src/modules/sharing/sharing.route.ts
@@ -1,17 +1,6 @@
 import { Router } from "express";
-import { requireAuth } from "../../middlewares/auth.middleware";
-import { requirePermission } from "../../middlewares/rbac.middleware";
-import { downloadDocumentSharedLink, viewDocumentSharedLink } from "./sharing.controller";
-
 const router = Router()
 
-
-router
-    .route("/shared/:token")
-    .get(requireAuth, requirePermission, viewDocumentSharedLink)
-
-router
-    .route("/shared/:token/download")
-    .get(requireAuth, requirePermission, downloadDocumentSharedLink)
+// location /documents/sharing
 
 export default router
