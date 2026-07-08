583f1c9f2b9d19d63d32daa46377c0fcdf519fac
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2026-07-08 12:42:29 +0500

    feat: implement comment management functionality with CRUD operations and permissions

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,55 +1,96 @@
 import { Request, Response } from "express";
-import asyncHandler from "../../utils/asyncHandler";
-import { ApiResponse } from "../../utils/ApiResponse";
+import asyncHandler from "@/utils/asyncHandler";
+import { ApiResponse } from "@/utils/ApiResponse";
 import { addDocumentCommentService, deleteCommentService, getDocumentCommentsService, updateCommentService } from "./comment.service";
 
+/**
+ * @description Get all comments for a document
+ * @route GET /api/v1/comments/:documentId
+ * @access Private
+ */
 export const getDocumentComments = asyncHandler(async (req: Request, res: Response) => {
-    const comments = await getDocumentCommentsService();
-    
-    res
+    const documentId = Number(req.params.documentId);
+
+    const comments = await getDocumentCommentsService(documentId);
+
+    return res
         .status(200)
         .json(
             new ApiResponse(
                 200,
+                { comments },
                 "Comments retrieved successfully"
             )
         );
 })
 
+/**
+ * @description Add a comment to a document
+ * @route POST /api/v1/comments/:documentId
+ * @access Private
+ */
 export const addDocumentComment = asyncHandler(async (req: Request, res: Response) => {
-    const comment = await addDocumentCommentService();
+    const documentId = Number(req.params.documentId);
+    const userId = Number(req.user?.id);
+    const content = req.body.content;
 
-    res
+    const comment = await addDocumentCommentService({
+        documentId,
+        userId,
+        content,
+    });
+
+    return res
         .status(200)
         .json(
             new ApiResponse(
                 200,
+                { comment },
                 "Comment added successfully"
             )
         );
 })
 
+/**
+ * @description Update a comment
+ * @route PUT /api/v1/comments/:id
+ * @access Private
+ */
 export const updateComment = asyncHandler(async (req: Request, res: Response) => {
-    const comment = await updateCommentService();
-    
-    res
+    const commentId = Number(req.params.commentId)
+    const userId = Number(req.user?.id)
+    const content = req.body.content;
+
+    const comment = await updateCommentService({ userId, commentId, content });
+
+    return res
         .status(200)
         .json(
             new ApiResponse(
                 200,
+                { comment },
                 "Comment updated successfully"
             )
         );
 })
 
+/**
+ * @description Delete a comment
+ * @route DELETE /api/v1/comments/:id
+ * @access Private
+ */
 export const deleteComment = asyncHandler(async (req: Request, res: Response) => {
-    const comment = await deleteCommentService();
-    
-    res
+    const commentId = Number(req.params.commentId);
+    const userId = Number(req.user?.id);
+
+    const comment = await deleteCommentService({ commentId, userId });
+
+    return res
         .status(200)
         .json(
             new ApiResponse(
                 200,
+                { comment },
                 "Comment deleted successfully"
             )
         );
